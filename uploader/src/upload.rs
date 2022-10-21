7921bf8bbc23e76d220ccb8303553f1130ae4dba
Islam El-Ashi <islam.elashi@dfinity.org>
2022-10-21 06:48:38 +0000

    updates

--- uploader/src/upload.rs
+++ uploader/src/upload.rs
@@ -55,12 +55,13 @@ async fn upload(agent: &Agent, canister_id: &Principal, page_start: u64, bytes: 
 
 #[async_std::main]
 async fn main() {
-    let f = File::open("testnet_stable_memory.bin").unwrap();
+    let f = File::open("testnet_stable_memory-run2.bin").unwrap();
     let mut reader = BufReader::new(f);
 
+    println!("creating agent");
     // Send some bytes to the canister.
     let agent = Agent::builder()
-        .with_url("http://127.0.0.1:8000/")
+        .with_url("https://ic0.app")
         //.with_identity(create_identity())
         .build()
         .expect("agent creation must succeed");
@@ -69,16 +70,17 @@ async fn main() {
     // This is important as the main net public key is static and a rogue network could return
     // a different key.
     // If you know the root key ahead of time, you can use `agent.set_root_key(root_key)?;`.
-    agent.fetch_root_key().await.expect("fetch root key failed");
+    //    agent.fetch_root_key().await.expect("fetch root key failed");
 
     let canister_id =
-        Principal::from_str("rrkah-fqaaa-aaaaa-aaaaq-cai").expect("invalid canister id");
+        Principal::from_str("g4xu7-jiaaa-aaaan-aaaaq-cai").expect("invalid canister id");
 
     let waiter = garcon::Delay::builder()
         .throttle(std::time::Duration::from_millis(500))
         .timeout(std::time::Duration::from_secs(60 * 5))
         .build();
 
+    println!("fetching missing pages");
     let response: Vec<u8> = agent
         .query(&canister_id, "get_missing_ranges")
         .with_arg(Encode!(&Empty).unwrap())
@@ -92,15 +94,14 @@ async fn main() {
 
     // TODO: only upload missing pages.
 
-    let mut offset = 4092;
     let mut buf = vec![0; 64 * 1024 * 31]; // 31 pages.
-    reader.seek(SeekFrom::Start(4092 * 64 * 1024));
-    while let Ok(bytes_read) = reader.read(&mut buf) {
-        if bytes_read == 0 {
-            break;
-        }
-        println!("uploading pages at {}", offset);
-        upload(&agent, &canister_id, offset, &buf[..bytes_read]).await;
-        offset += 31;
+    for missing_page in missing_pages {
+        println!("uploading pages at {}", missing_page);
+        reader
+            .seek(SeekFrom::Start(missing_page * 64 * 1024))
+            .unwrap();
+        let bytes_read = reader.read(&mut buf).unwrap();
+//        assert_eq!(bytes_read, 31 * 64 * 1024); // assert except for last page.
+        upload(&agent, &canister_id, missing_page, &buf[..bytes_read]).await;
     }
 }
